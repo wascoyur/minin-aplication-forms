8c5cbb80c1c939a3112e3bd449ad4d46d1011f44
wascoyur <mytem20007@gmail.com>
2020-05-18 21:32:31 +0500

    перед валидацией формы

--- src/app.js
+++ src/app.js
@@ -1,2 +1,9 @@
 import './styles.css'
-console.log('App working..');
+const form = document.getElementById('form');
+const input = form.querySelector('#question-input');
+const btnSend = form.querySelector('#submit');
+form.addEventListener('submit', submitFormHandler)
+function submitFormHandler (event){
+	event.preventDefault();
+	console.log(input.value);
+}
