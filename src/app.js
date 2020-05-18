2f5985fd73daa69ab562d79f3b279cdb7b6ccac9
wascoyur <mytem20007@gmail.com>
2020-05-18 21:55:01 +0500

    break 33:46

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 import './styles.css'
+import {isValid} from './utils';
 const form = document.getElementById('form');
 const input = form.querySelector('#question-input');
 const btnSend = form.querySelector('#submit');
 form.addEventListener('submit', submitFormHandler)
 function submitFormHandler (event){
 	event.preventDefault();
-	console.log(input.value);
+	if (isValid(input.value)) {
+		const question = {
+			text: input.value.trim(),
+			date: new Date().toJSON(),
+		}
+	}
 }
